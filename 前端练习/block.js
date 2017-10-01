f0bd80eaf94400b35b954b9528c7c1d7a5ae4c9c
YeChang <337894180@qq.com>
2017-10-01 21:45:39 +0800

    add debug mode and load level

--- 前端练习/block.js
+++ 前端练习/block.js
@@ -1,18 +1,24 @@
-var Block = function () {
+var Block = function (position) {
+    //position 是 [0, 0] 格式
+    var p = position
     var image = imageFromPath('block.png')
     var o = {
         img: image,
-        x: 20,
-        y: 20,
+        x: p[0],
+        y: p[1],
         w: 50,
         h: 20,
-        lifes: true,
+        alive: true,
+        hp: p[2] || 1
     }
     o.kill = function () {
-        o.lifes = false
+        o.hp--
+        if (o.hp < 1) {
+            o.alive = false
+        }
     }
     o.collide = function (b) {
-        return o.lifes && ((aInb(o, b) || aInb(b, o)))
+        return o.alive && ((aInb(o, b) || aInb(b, o)))
     }
     return o
 }
