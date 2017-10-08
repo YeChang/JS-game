5dca9aa27d1b93fd155b0cd00ada93ca774e20bf
huihui666 <337894180@qq.com>
2017-10-08 14:41:33 +0800

    先提交 同步一下

--- flappybird/guagame/gua_animation.js
+++ flappybird/guagame/gua_animation.js
@@ -1,45 +1,50 @@
-// class GuaAnimation {
-//     constructor(game) {
-//         this.game = game
-//         this.animations = {
-//             idle: [],
-//             run: [],
-//         }
-//         for (var i = 1; i < 7; i++) {
-//             var name = 'run' + i.toString()
-//             log('name', name)
-//             var t = game.textureByName(name)
-//             this.animations['run'].push(t)
-//         }
-//         for (var i = 1; i < 7; i++) {
-//             var name = 'idle' + i.toString()
-//             log('name', name)
-//             var t = game.textureByName(name)
-//             this.animations['idle'].push(t)
-//         }
-//         this.animationName = 'idle'
-//         this.texture = this.frames()[0]
-//         this.w = this.texture.width
-//         this.h = this.texture.height
-//         this.frameIndex = 0
-//         this.frameCount = 5
-//         //
-//         this.flipX = false
-//     }
-//     static new(game) {
-//         return new this(game)
-//     }
-//     frames() {
-//         retrurn this.animations[this.animationName]
-//     }
-//     update() {
-//         this.frameCount--
-//         if (this.frameCount == 0) {
-//             this.frameCount = 5
-//             this.frameIndex = (this.frameIndex + 1) % this.frames().length
-//             this.texture = this.frames()[this.frameIndex]
-//         }
-//     }
+class GuaAnimation {
+    constructor(game) {
+        this.game = game
+        this.animations = {
+            idle: [],
+            run: [],
+        }
+        for (var i = 1; i < 7; i++) {
+            var name = 'run' + i.toString()
+            log('name', name)
+            var t = game.textureByName(name)
+            this.animations['run'].push(t)
+        }
+        for (var i = 1; i < 7; i++) {
+            var name = 'idle' + i.toString()
+            log('name', name)
+            var t = game.textureByName(name)
+            this.animations['idle'].push(t)
+        }
+        this.animationName = 'idle'
+        this.texture = this.frames()[0]
+        this.w = this.texture.width
+        this.h = this.texture.height
+        this.frameIndex = 0
+        this.frameCount = 5
+        //
+        this.flipX = false
+
+        this.gy = 10
+        this.vy = 0
+    }
+    static new(game) {
+        return new this(game)
+    }
+    frames() {
+        retrurn this.animations[this.animationName]
+    }
+    update() {
+        this.y += this.vy
+        this.vy += this.gy * 0.2
+        this.frameCount--
+        if (this.frameCount == 0) {
+            this.frameCount = 5
+            this.frameIndex = (this.frameIndex + 1) % this.frames().length
+            this.texture = this.frames()[this.frameIndex]
+        }
+    }
 //     draw() {
 //         context = this.game.context
 //         if (this.flipX) {
